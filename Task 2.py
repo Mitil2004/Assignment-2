70fbaaa403b8570fec0b4c0c4ca3593762fb1171
Mitil2004 <mitilbondre@gmail.com>
2025-05-29 01:25:23 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,5 @@
+total = 0
+for i in range(1,51):
+  total +=i
+
+print("sum of integers in 1-50 is",total)
---
+++ Task1.py
@@ -0,0 +1,8 @@
+x=int(input("enter your number"))
+
+
+if x % 2== 0:
+  print("The number is Even")
+else:
+  print("The number is Odd")
+  print('Thank you')
